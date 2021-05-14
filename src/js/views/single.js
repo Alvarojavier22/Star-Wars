f06727f50e59b9dcbe0128714bd680b7fff5994e
Alvaro Javier <alvarojavierchagas@hotmail.com>
2021-05-14 18:24:57 +0000

    agregue nombre

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -17,7 +17,7 @@ export const Single = props => {
 						alt="..."
 					/>
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						<h5 className="card-title">{store.personabiografia.name}</h5>
 						<p className="card-text">
 							This is a wider card with supporting text below as a natural lead-in to additional content.
 							This content is a little bit longer.
